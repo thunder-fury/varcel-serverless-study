8d0977efde3da45a0481df4c1b1e3bef1eb92e04
woohyeonwoo <>
2022-06-11 16:22:38 +0900

    fix sample api

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 3090
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  try {
+    res.send({ name: "hoge" })
+  } catch (error) {
+    res.sendStatus(500)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+export default app
